cf25fbac3bf42be228b20d86d9b14566a7632b5c
tainamiranda2 <tainamirandaferreira@gmail.com>
2022-11-16 09:49:00 -0300

    incrementanod mias coisas

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -5,7 +5,14 @@ import { Button } from "../../components/button/button";
 import './admin.css'
 
 import {FiTrash2} from 'react-icons/fi'
+import { useState } from "react";
 export default function Admin(){
+    const [nameInput, setNameInput]=useState("")
+    const [urlInput, seturlInput]=useState("")
+    const [backgroundColorInput, setbackgroundColorInput]=useState("#fff")
+    const [textColorInput, settextColorInput]=useState("#000")
+    
+   //previwe
     return(
         <>
         <Header/>
@@ -14,23 +21,47 @@ export default function Admin(){
    
         <form  className="form">
             <label  className="label">Nome do link</label>
-            <Input type="text" placeholder="Nome do link"/>
+            <Input 
+            type="text"
+            name="nameInput" 
+            placeholder="Nome do link"
+            value={nameInput}
+            onChange={(e)=>setNameInput(e.target.value)}
+            />
             <label  className="label">Url do link</label>
-            <Input type="url" placeholder="Url do link"/>
+            <Input 
+            type="url" 
+            placeholder="Url do link"
+            value={urlInput}
+            onChange={(e)=>seturlInput(e.target.value)}
+            />
             <section className="container-colors">
                 <div>
-<label className="label right">Fund do link</label>
-<Input type="color"/>
+        <label className="label right">  Fund do link  </label>
+            <Input type="color"
+            value={backgroundColorInput}
+            onChange={(e)=>setbackgroundColorInput(e.target.value)}
+            />
                 </div>
                 <div>
-<label className="label right">cor do link</label>
-<Input type="color"/>
+        <label className="label right">cor do link</label>
+                <Input type="color"
+                value={textColorInput}
+                onChange={(e)=>settextColorInput(e.target.value)}
+                />
                 </div>
             </section>
+            <div className="previws">
+            <label className="label">Veja como esta ficando</label>
+            <article className="list" style={{backgroundColor: backgroundColorInput}}>
+            <p style={{color: textColorInput}}> {nameInput}</p>
+            </article>
+            </div>
+
            <Button text="cadastrar"/>
         </form>
         <h2 className="title">Meus links</h2>
-        <article>
+        <article className="links">
             <p>Grupo exclusiva do telegram</p>
             <div>
                 <button  className="btn-delete">
